591950c736edeb09289c4670ddb8924ef2275565
Abt, Daniel <dabt@irobot.com>
2024-11-15 16:36:57 -0300

    Add localstack

---
+++ apps/ip_app/increment_patch.py
@@ -0,0 +1,16 @@
+def increment_patch(file_path="apps/ip_app/version.txt"):
+    # Get the current version from file
+    with open(file_path, "r") as version_file:
+        current_version = version_file.read().strip()
+
+    # split version: MAJOR, MINOR, PATCH
+    major, minor, patch = map(int, current_version.split("."))
+
+    patch += 1
+    new_version = f"{major}.{minor}.{patch}"
+
+    print(f"Updated version: {new_version}")
+    return new_version
+
+if __name__ == "__main__":
+    increment_patch()
